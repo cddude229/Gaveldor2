ff64586d1c495a6a308e1fb6ae02ca1a0ac6b11d
Jeremy <jesharpe92@gmail.com>
2012-10-19 15:44:53 -0400

    Added Constants.java

---
+++ Gaveldor 2/src/game/model/Constants.java
@@ -0,0 +1,18 @@
+package game.model;
+
+/*
+ * Global constants file
+ */
+public class Constants {
+
+    // Action.java
+    
+    // Constants.java
+    
+    // GameModel.java
+    
+    // Piece.java
+    
+    // Player.java
+    
+}
