cd804632efad9c7c25bb74314c60e35cc42a9cbf
Benjamin Greenberg <bengreenberg@dhcp-18-111-11-130.dyn.mit.edu>
2012-10-22 20:52:32 -0400

    archer in progress and pieces can now move distance 2 if they have move range of 2

---
+++ Gaveldor 2/src/game/model/Archer.java
@@ -0,0 +1,54 @@
+package game.model;
+
+import org.newdawn.slick.Image;
+
+import util.Constants;
+
+public class Archer extends Piece {
+    private int health, attackPower, attackRange, moveRange;
+    public Archer(Player owner, Point p) {
+        super(owner, p);
+        this.health = Constants.ARCHER_HEALTH_POINTS;
+        this.attackPower = Constants.ARCHER_ATTACK_POWER;
+        this.attackRange = Constants.ARCHER_ATTACK_RANGE;
+        this.moveRange = Constants.ARCHER_MOVE_RANGE;
+    }
+
+    @Override
+    public void attack(Piece opponent) {
+        int power = this.defaultAttackPower();
+
+        if (this.isBackAttack(opponent))
+            power*=2;
+
+        opponent.loseHealth(power);
+        
+    }
+
+    @Override
+    public int defaultHealth() {
+        return this.health;
+    }
+
+    @Override
+    public int defaultAttackPower() {
+        return this.attackPower;
+    }
+
+    @Override
+    public int defaultAttackRange() {
+        return this.attackRange;
+    }
+
+    @Override
+    public int defaultMoveRange() {
+        return this.moveRange;
+    }
+
+    @Override
+    public Image getSprite() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
--- Gaveldor 2/src/game/model/Piece.java
+++ Gaveldor 2/src/game/model/Piece.java
@@ -6,6 +6,7 @@ public abstract class Piece {
     private int currentHealth, currentDirection;
     private Point point;
     public final Player owner;
+    private int health, attackPower, attackRange, moveRange;
     
     /**
      * Only pass in the default position.
@@ -152,15 +153,42 @@ public abstract class Piece {
     final public Point[] getValidMoves(){
         // TODO: This only takes into account dist=1, not dist=2.
         Point p = this.getPosition();
-        Point[] ret = {
-            new Point(p.x,   p.y-2),
-            new Point(p.x+1, p.y-1),
-            new Point(p.x+1, p.y+1),
-            new Point(p.x,   p.y+2),
-            new Point(p.x-1, p.y+1),
-            new Point(p.x-1, p.y-1)
-        };
-        return ret; 
+        if (this.moveRange==1)
+        {
+            Point [] ret = {
+                new Point(p.x,   p.y-2),
+                new Point(p.x+1, p.y-1),
+                new Point(p.x+1, p.y+1),
+                new Point(p.x,   p.y+2),
+                new Point(p.x-1, p.y+1),
+                new Point(p.x-1, p.y-1)
+            };
+            return ret;
+        }
+        else
+        {
+            Point [] ret = {
+                    new Point(p.x, p.y-4),  //0
+                    new Point(p.x+1,p.y-3), //.5
+                    new Point(p.x+2,p.y-2), //1
+                    new Point(p.x+2,p.y),   //1.5
+                    new Point(p.x+2,p.y+2), //2
+                    new Point(p.x+1,p.y+3), //2.5
+                    new Point(p.x,p.y+4),   //3
+                    new Point(p.x-1,p.y+3), //3.5
+                    new Point(p.x-2,p.y+2), //4
+                    new Point(p.x-2,p.y),   //5
+                    new Point(p.x-2,p.y-2), //5.5
+                    new Point(p.x-1,p.y-3), //6
+                    new Point(p.x,   p.y-2), //move length 1
+                    new Point(p.x+1, p.y-1),
+                    new Point(p.x+1, p.y+1),
+                    new Point(p.x,   p.y+2),
+                    new Point(p.x-1, p.y+1),
+                    new Point(p.x-1, p.y-1)
+            };
+            return ret;
+        }
     }
     
     /**
