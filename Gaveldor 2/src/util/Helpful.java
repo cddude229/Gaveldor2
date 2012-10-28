5f1d37646c7d383d6df2e7ff4e3d352b990e66ee
Chris Dessonville <cdville@mit.edu>
2012-10-28 15:31:58 -0400

    Fixing piece for new grid system.  Refactored partially with helper methods.

---
+++ Gaveldor 2/src/util/Helpful.java
@@ -0,0 +1,20 @@
+package util;
+
+import java.util.Arrays;
+
+public class Helpful {
+    // FROM: http://stackoverflow.com/questions/80476/how-to-concatenate-two-arrays-in-java
+    public static <T> T[] arrayConcatAll(T[] first, T[]... rest) {
+        int totalLength = first.length;
+        for (T[] array : rest) {
+            totalLength += array.length;
+        }
+        T[] result = Arrays.copyOf(first, totalLength);
+        int offset = first.length;
+        for (T[] array : rest) {
+            System.arraycopy(array, 0, result, offset, array.length);
+            offset += array.length;
+        }
+        return result;
+    }
+}
