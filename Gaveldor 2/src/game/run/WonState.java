6dde99ae6b65bcde5b314da30a2bb2fd760b68c6
Todd Layton <tlayton@mit.edu>
2012-10-31 15:41:08 -0400

    added WonState

---
+++ Gaveldor 2/src/game/run/WonState.java
@@ -0,0 +1,31 @@
+package game.run;
+
+import game.model.GameModel.GameState;
+
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+
+public class WonState extends PlayerControllerState {
+
+    public WonState(boolean isLocal) {
+        super(GameState.WON, isLocal);
+    }
+
+    @Override
+    public void init(GameContainer container, PlayerController pc) throws SlickException {
+    }
+
+    @Override
+    public void render(GameContainer container, PlayerController pc, Graphics g) throws SlickException {
+        pc.renderBoard(g);
+        pc.renderPieces(g);
+        pc.renderControllerWon(g);
+    }
+
+    @Override
+    public void updateLocal(GameContainer container, LocalPlayerController pc, int delta) throws SlickException {
+        //TODO
+    }
+
+}
