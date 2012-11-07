084021b41991860fa29e7c173a62a690386e9299
Jeremy <jesharpe92@gmail.com>
2012-11-07 12:19:36 -0500

    conflict

--- Gaveldor 2/src/run/MainMenuState.java
+++ Gaveldor 2/src/run/MainMenuState.java
@@ -8,13 +8,12 @@ import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
+import org.newdawn.slick.Music;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.Sound;
 import org.newdawn.slick.geom.Rectangle;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
-
-import util.Constants;
 
 import com.aem.sticky.StickyListener;
 import com.aem.sticky.button.Button;
@@ -36,6 +35,8 @@ public class MainMenuState extends BasicGameState {
      * Builds buttons and adds listeners to game. This isn't fully functional.
      */
     public void init(GameContainer container, StateBasedGame game) throws SlickException {
+        Music music = new Music("assets/audio/music/DarkKnight.ogg");
+        music.loop();
         this.container = container;
         this.game = game;
         listener = new StickyListener();
@@ -83,8 +84,8 @@ public class MainMenuState extends BasicGameState {
      * @param height
      * @return an int[] containing the screen location of the buttons
      */
-    public int getxLoc(int width) {
-        int scnWidth = Constants.WINDOW_WIDTH;
+    public int getxLoc(GameContainer container, int width) {
+        int scnWidth = container.getWidth();
         int xLoc = scnWidth / 2 - width / 2;
         return xLoc;
     }
@@ -102,7 +103,7 @@ public class MainMenuState extends BasicGameState {
         ArrayList<int[]> locations = new ArrayList<int[]>();
         int yLoc = 75;
         for (int i = 0; i < 8; i++) {
-            locations.add(new int[] { this.getxLoc(bWidth), yLoc });
+            locations.add(new int[] { this.getxLoc(container, bWidth), yLoc });
             yLoc += 75;
         }
         
